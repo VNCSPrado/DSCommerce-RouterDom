2885e51af76fb55c50cf3212749f2347f10444ef
Vinicius Prado <viniciusprado.prado@gmail.com>
2024-08-30 13:53:37 -0300

    dinamics Product and Catalog

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -6,30 +6,7 @@ import HeaderClient from '../../../components/HeaderClient';
 import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
-import { ProductDTO } from '../../../models/product';
-
-const product: ProductDTO = {
-  id: 2,
-  name: 'Smart TV',
-  description: 'Que TV bonitona ein!',
-  imgUrl:
-    'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-  price: 2500.99,
-  categories: [
-    {
-      id: 1,
-      name: 'Eletronicos',
-    },
-    {
-      id: 2,
-      name: 'Computadores',
-    },
-    {
-      id: 3,
-      name: 'Importados',
-    },
-  ],
-};
+import * as productService from '../../../services/product-service';
 
 function Catalog() {
   return (
@@ -38,18 +15,9 @@ function Catalog() {
         <section id="catalog-section" className="dsc-container">
           <SearchBar />
           <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
+            {productService.findAll().map((product) => (
+              <CatalogCard key={product.id} product={product} />
+            ))}
           </div>
           <ButtonNextPage />
         </section>
